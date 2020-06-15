f8ba6ae5e0f253c39292bbe30cb95f0469681bb8
Shii <isihibi1456@icloud.com>
2020-06-16 00:54:31 +0900

    change num 10000 to 50

--- hist.py
+++ hist.py
@@ -2,7 +2,7 @@ from scipy.stats import norm
 import numpy as np
 import matplotlib.pyplot as plt
 
-num = 10000
+num = 50
 N = 10
 mean, std = 2, 0.5
 mu = np.zeros(num)
@@ -10,5 +10,5 @@ mu = np.zeros(num)
 for i in range(num) :
     mu[i] = np.mean(norm.rvs(loc=mean, scale=std, size=N))
 
-plt.hist(mu, bins=20, range=(1.5, 2.5), ec='black')
+plt.hist(mu, bins=20, range=(1.5, 2.5))
 plt.show()
